4068b544b16b20b6db95caac2340a639ce57be5b
GreenPotionDevelopment <greenpotiondevelopment@gmail.com>
2019-06-01 23:05:50 +0200

    Initial commit

---
+++ Core/Slicer/Slice.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpriteSlicer.Core.Slicer
+{
+    class Slicer
+    {
+
+        public class Slices
+        {
+            public List<Slice> slices = new List<Slice>();
+
+            public class Slice
+            {
+                public int part { get; set; }
+                public Bitmap getBitmap { get; set; }
+            }
+        }
+
+        private static Bitmap temp_BMP;
+
+        public static Bitmap GetBitmap
+        {
+            get
+            {
+                return temp_BMP;
+            }
+            set
+            {
+                value.MakeTransparent();
+                temp_BMP = value;
+            }
+        }
+
+        public static void LoadImage(string file)
+        {
+            GetBitmap = new Bitmap(Image.FromFile(file));
+        }
+
+        public static Slices SliceImage(int SliceWidth, int SliceHeight)
+        {
+            Slices _slices = new Slices();
+            int p1 = GetBitmap.Width / SliceWidth;
+            int p2 = GetBitmap.Height / SliceHeight;
+            for (int y = 0; y < p2; y++)
+            {
+                for (int x = 0; x < p1; x++)
+                {
+                    int valX = x * SliceWidth;
+                    int valY = y * SliceHeight;
+
+                    var rect = new Rectangle
+                    {
+                        Width = SliceWidth,
+                        Height = SliceHeight,
+                        Size = new Size(SliceWidth, SliceHeight),
+                        Location = new Point(valX,valY),
+                        X = valX,
+                        Y = valY,
+                    };
+
+                    var bmp = GetBitmap.Clone(rect, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                    Slices.Slice slc = new Slices.Slice
+                    {
+                        getBitmap = bmp,
+                        part = x,
+                    };
+                    _slices.slices.Add(slc);
+                }
+            }
+            return _slices;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpriteSlicer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Enter Sprite File Path: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string pth = Console.ReadLine();
+             pth = pth.Replace("\"", "");
+            SpriteSlicer.Core.Slicer.Slicer.LoadImage(pth);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Split by Width: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string x = Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Split by Height: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string y = Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Are you sure you want to split by {x}x{y}");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            var tp = SpriteSlicer.Core.Slicer.Slicer.SliceImage(64, 64);
+            foreach (SpriteSlicer.Core.Slicer.Slicer.Slices.Slice slc in tp.slices.FindAll(img => img.getBitmap != null))
+            {
+                slc.getBitmap.MakeTransparent();
+                slc.getBitmap.Save($"{Path.GetDirectoryName(pth)}/split_{slc.part}.png", System.Drawing.Imaging.ImageFormat.Png);
+            }
+            Console.WriteLine("Successfully split image");
+            Console.ReadLine();
+        }
+    }
+}
